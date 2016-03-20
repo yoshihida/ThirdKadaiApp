0db13444e07f7fa7cbfde10cacad6656ed3b43ec
Yoshifumi Hidaka <leafrhino@mac-no-macbook-air.local>
2016-03-20 22:13:00 +0900

    Initial Commit

---
+++ ThirdKadaiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThirdKadaiApp
+//
+//  Created by Yoshifumi Hidaka on 2016/03/20.
+//  Copyright © 2016年 Yoshifumi Hidaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
